a574183531e279d7e03e63737aca64c8b5e8c61c
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-05-20 10:12:48 -0700

    creating Error Logger class

--- Todo.js
+++ Todo.js
@@ -13,4 +13,32 @@ class Todo {
     set title(newTitle) {
         this._title = newTitle;
     }
+
+    get title() {
+        return this_title;
+    }
+
+    set description(newDes) {
+        this._description = newDes;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set dueDate(newDueDate) {
+        this._dueDate = newDueDate;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    set priority(newPriority) {
+        this._priority = newPriority;
+    }
+
+    get priority() {
+        return this._priority;
+    }
 }
